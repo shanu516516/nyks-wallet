b00aece024ec9f87d98b3ba39bdf55bdd80366a0
Siddharth <20308880+shanu516516@users.noreply.github.com>
2026-04-16 05:08:56 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-REPO="twilight-project/nyks-wallet"
+REPO="shanu516516/nyks-wallet"
 API_URL="https://api.github.com/repos/${REPO}/releases/latest"
 INSTALL_DIR="."
 BINARY_NAME="relayer-cli"
